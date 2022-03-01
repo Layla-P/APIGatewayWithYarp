cc32484b1a61fbe47eebae789e7e6969a503501c
layla-p <layla.porter@gmail.com>
2022-03-01 10:45:18 +0000

    completed code

--- Code/Client/Program.cs
+++ Code/Client/Program.cs
@@ -8,12 +8,12 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient("FoodService", client =>
 {
-	client.BaseAddress = new Uri("https://localhost:5011/");
+	client.BaseAddress = new Uri("https://localhost:5001/foodservice/");
 });
 
 builder.Services.AddHttpClient("DrinkService", client =>
 {
-	client.BaseAddress = new Uri("https://localhost:5021/");
+	client.BaseAddress = new Uri("https://localhost:5001/drinkservice/");
 });
 
 
--- Code/Gateway/Program.cs
+++ Code/Gateway/Program.cs
@@ -1,25 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+var proxyBuilder = builder.Services.AddReverseProxy();
+// Initialize the reverse proxy from the "ReverseProxy" section of configuration
+proxyBuilder.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
 
-app.MapRazorPages();
+app.UseEndpoints(endpoints =>
+{
+	endpoints.MapReverseProxy();
+
+});
+
 
 app.Run();
